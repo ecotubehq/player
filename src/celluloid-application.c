96802bd21d6b66dd9e207941045e3e67a97b3a19
Sako <sakoadam@googlemail.com>
2026-04-19 15:50:27 -0400

    update flathub notice

--- src/celluloid-application.c
+++ src/celluloid-application.c
@@ -234,11 +234,11 @@ initialize_gui(CelluloidApplication *app)
 	g_settings_set_boolean(settings, "always-use-floating-header-bar", TRUE);
 	g_settings_set_boolean(settings, "always-use-floating-controls", TRUE);
 
-	gboolean notif = g_settings_get_boolean(settings, "startup-version-notif-26-03-01");
+	gboolean notif = g_settings_get_boolean(settings, "startup-version-notif-26-04-01");
 	if(!notif){
 		celluloid_view_show_message_toast(view, "Ecotube updated - See 'How To Use' for details");
 		load_default_scripts();
-		g_settings_set_boolean(settings, "startup-version-notif-26-03-01", TRUE);
+		g_settings_set_boolean(settings, "startup-version-notif-26-04-01", TRUE);
 	}
 	if(is_modern_osd()){
 		celluloid_main_window_set_use_floating_controls(window, FALSE);
